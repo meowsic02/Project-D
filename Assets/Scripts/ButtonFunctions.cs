ab5fabd1bddf579ff239b6c2dae6f8d28301f2c1
wnstjq04 <57716883+wnstjq04@users.noreply.github.com>
2019-11-16 00:05:44 +0900

    Added JumpCount and EscMenu

---
+++ Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public TextMeshProUGUI TMPugui;
+
+    public void OnPointerEnter()
+    {
+        TMPugui.color = new Color32(255, 255, 0, 255);
+    }
+    public void OnPointerExit()
+    {
+        TMPugui.color = new Color32(255, 255, 255, 255);
+    }
+    public void QuitButtonClick()
+    {
+        Debug.Log("QuitButton Click");
+    }
+    public void OptionsButtonClick()
+    {
+        Debug.Log("OptionsButton Click");
+    }
+    public void ExitButtonClick()
+    {
+        Debug.Log("ExitButton Click");
+    }
+}
---
+++ Assets/Scripts/PlayerDetector2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerDetector2 : MonoBehaviour
+{
+    public PlayerController Player;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8 || collision.gameObject.layer == 9)
+        {
+            Player.JumpCountLeft = Player.MaxJumpCount;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8 || collision.gameObject.layer == 9)
+        {
+            Player.JumpCountLeft = 0;
+        }
+    }
+}
